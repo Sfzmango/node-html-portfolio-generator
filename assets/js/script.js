f959b82f69902437b4f635a43cdf79d133e6a0e5
Asika Habib <asikahabib@gmail.com>
2022-04-20 12:47:33 -0700

    added npm inquirer

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$("body").append().html(`
+<div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">name</h1>
+            <p>location</p>
+            <p class="col-md-8 fs-4">bio</p>
+            <button class="btn btn-primary btn-lg" type="button">LinkedIn</button>
+            <button class="btn btn-primary btn-lg" type="button">GitHub</button>
+        </div>
+    </div>
+`)
+
